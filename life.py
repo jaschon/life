69b153dbc6a18c862c0a6f233b21ae950a61a42a
Jason Rebuck <jaschon@users.noreply.github.com>
2023-04-03 15:38:32 -0700

    Update life.py

--- life.py
+++ life.py
@@ -57,10 +57,19 @@ class Life:
 if __name__ == "__main__":
     l = Life()
 
-    l.seed( (1,1), (2,1), (3,1)  ) #line
-    # w.seed( (0,1), (1,0), (2,0)  ) #broken line
-    # w.seed( (0,0), (0,1), (1,0)  ) #l corner
-    # w.seed( (0,2), (1,1), (2,0)  ) #r diag
+    # l.seed( (1,1), (2,1), (3,1)  ) #line
+    # l.seed( (4,1), (5,0), (6,0)  ) #broken line
+    # l.seed( (0,0), (0,1), (1,0)  ) #l corner
+    # l.seed( (0,2), (1,1), (2,0)  ) #r diag
+
+    # l.seed( (4,5), (5,4), (5,5), (5,6) ) # tetris L
+    # l.seed( (2,2), (2,3), (2,4), (3,2) ) #tetris T
+    # l.seed( (2,2), (2,3), (2,4), (2,5) ) #tetris I
+    # l.seed( (2,2), (2,3), (3,2), (3,3) ) #tetris O
+    # l.seed( (2,2), (2,3), (2,4), (3,2) ) #tetris J
+
+    # l.seed( (10,10), (10,11), (10,12), (11,10), (12,11) ) #glider
+    l.seed( (5,6), (5,7), (6,5), (6,6), (7,6) ) #F pentomino
 
     l.loop()
    
