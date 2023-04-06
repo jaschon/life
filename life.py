d96b6fb29d0929263003eadbe716833b43467bc3
Jason Rebuck <jaschon@users.noreply.github.com>
2023-04-06 12:04:33 -0700

    Update life.py

--- life.py
+++ life.py
@@ -8,18 +8,22 @@ class Life:
     mark = ("◻","◼")
 
     def __init__(self, size=20, sec=1):
+        """Init and set size, update time and build grid"""
         self.size = size
         self.sec = sec
         self.grid = self._build()
 
     def _build(self):
+        """Build a blank grid size*size"""
         return [[0 for _w in range(self.size)] for _h in range(self.size)]
 
     def seed(self, *vals):
+        """Mark a list of pairs (row from top, col from left)"""
         for v in vals:
             self.grid[v[0]][v[1]] = 1
 
     def count(self, row, col, amt=0):
+        """Count the number of neighbors"""
         for delta_row in (self.size - 1, 0, 1):
             for delta_col in (self.size - 1, 0, 1):
                 if delta_row == 0 and delta_col == 0: continue
@@ -29,12 +33,14 @@ class Life:
         return amt
 
     def draw(self):
+        """Output grid to screen"""
         for row in range(self.size):
             for col in range(self.size):
                 print(self.mark[self.grid[row][col]], end="")
             print()
 
     def next(self):
+        """Build next generation and swap current"""
         buf = self._build()
         for row in range(self.size):
             for col in range(self.size):
@@ -42,6 +48,7 @@ class Life:
         self.grid = buf
 
     def loop(self):
+        """Loop animate"""
         while 1:
             try:
                 print("\033[H \033[J")
@@ -53,21 +60,5 @@ class Life:
 
 
 if __name__ == "__main__":
-    l = Life()
-
-    # l.seed( (1,1), (2,1), (3,1)  ) #line
-    # l.seed( (4,1), (5,0), (6,0)  ) #broken line
-    # l.seed( (0,0), (0,1), (1,0)  ) #l corner
-    # l.seed( (0,2), (1,1), (2,0)  ) #r diag
-
-    # l.seed( (4,5), (5,4), (5,5), (5,6) ) # tetris L
-    # l.seed( (2,2), (2,3), (2,4), (3,2) ) #tetris T
-    # l.seed( (2,2), (2,3), (2,4), (2,5) ) #tetris I
-    # l.seed( (2,2), (2,3), (3,2), (3,3) ) #tetris O
-    # l.seed( (2,2), (2,3), (2,4), (3,2) ) #tetris J
-
-    # l.seed( (10,10), (10,11), (10,12), (11,10), (12,11) ) #glider
-    l.seed( (5,6), (5,7), (6,5), (6,6), (7,6) ) #F pentomino
-
-    l.loop()
+    pass
    
