d15557d47f51f1619f4293d177214f596c076d1b
Jason Rebuck <jaschon@users.noreply.github.com>
2023-04-06 11:56:49 -0700

    Update life.py

--- life.py
+++ life.py
@@ -5,43 +5,41 @@ from time import sleep
 class Life:
 
     rules = {(1,2): 1, (1,3): 1, (0,3): 1,}
-    mark = {0: "◻", 1: "◼"}
+    mark = ("◻","◼")
 
-    def __init__(self, height=20, width=20, seconds=1):
-        self.height = height
-        self.width = width
-        self.seconds = seconds
+    def __init__(self, size=20, sec=1):
+        self.size = size
+        self.sec = sec
         self.grid = self._build()
 
     def _build(self):
-        return [[0 for _ in range(self.width)] for _ in range(self.height)]
+        return [[0 for _w in range(self.size)] for _h in range(self.size)]
 
     def seed(self, *vals):
         for v in vals:
             self.grid[v[0]][v[1]] = 1
 
     def count(self, row, col, amt=0):
-        for delta_row in (self.height - 1, 0, 1):
-            for delta_col in (self.width-1, 0, 1):
+        for delta_row in (self.size - 1, 0, 1):
+            for delta_col in (self.size - 1, 0, 1):
                 if delta_row == 0 and delta_col == 0: continue
-                n_row = (row+delta_row) % self.height
-                n_col = (col+delta_col) % self.width
+                n_row = (row+delta_row) % self.size
+                n_col = (col+delta_col) % self.size
                 amt += self.grid[n_row][n_col]
         return amt
 
     def draw(self):
-        for row in range(self.height):
-            for col in range(self.width):
-                print(self.mark.get(self.grid[row][col]), end="")
+        for row in range(self.size):
+            for col in range(self.size):
+                print(self.mark[self.grid[row][col]], end="")
             print()
 
     def next(self):
-        frame = self._build()
-        for row in range(self.height):
-            for col in range(self.width):
-                frame[row][col] = self.rules.get(\
-                        (self.grid[row][col], self.count(row,col)),0)
-        self.grid = frame
+        buf = self._build()
+        for row in range(self.size):
+            for col in range(self.size):
+                buf[row][col] = self.rules.get((self.grid[row][col], self.count(row,col)),0)
+        self.grid = buf
 
     def loop(self):
         while 1:
@@ -49,7 +47,7 @@ class Life:
                 print("\033[H \033[J")
                 self.draw()
                 self.next()
-                sleep(self.seconds)
+                sleep(self.sec)
             except KeyboardInterrupt:
                 exit()
 
